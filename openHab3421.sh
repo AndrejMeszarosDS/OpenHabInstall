c7d5cbb5793e4e6e27dcf9c0f205d0467e14262b
AndrejMeszarosDS <78271014+AndrejMeszarosDS@users.noreply.github.com>
2024-03-26 08:28:03 +0100

    add Yes to install

--- openHab3421.sh
+++ openHab3421.sh
@@ -1,16 +1,17 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install openjdk-17-jre-headless
+sudo apt install -y openjdk-17-jre-headless
 curl -fsSL "https://openhab.jfrog.io/artifactory/api/gpg/key/public" | gpg --dearmor > openhab.gpg
 sudo mkdir /usr/share/keyrings
 sudo mv openhab.gpg /usr/share/keyrings
 sudo chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg
 echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
-sudo apt-get update
-sudo apt install openhab=3.4.2-1
+sudo apt-get -y update
+sudo apt install -y openhab=3.4.2-1
 sudo apt-mark hold openhab
 sudo apt-mark hold openhab-addons
 sudo systemctl start openhab.service
 sudo systemctl status openhab.service
 sudo systemctl daemon-reload
 sudo systemctl enable openhab.service
+openhab-cli info
