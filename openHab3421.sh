d255c1856c1d704e315183be030673f8455a057a
AndrejMeszarosDS <78271014+AndrejMeszarosDS@users.noreply.github.com>
2024-03-26 08:48:26 +0100

    force yes

--- openHab3421.sh
+++ openHab3421.sh
@@ -1,13 +1,13 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install -y openjdk-17-jre-headless
+sudo apt install --yes --force-yes openjdk-17-jre-headless
 curl -fsSL "https://openhab.jfrog.io/artifactory/api/gpg/key/public" | gpg --dearmor > openhab.gpg
 sudo mkdir /usr/share/keyrings
 sudo mv openhab.gpg /usr/share/keyrings
 sudo chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg
 echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
-sudo apt-get -y update
-sudo apt install -y openhab=3.4.2-1
+sudo apt-get --yes --force-yes update
+sudo apt install --yes --force-yes openhab=3.4.2-1
 sudo apt-mark hold openhab
 sudo apt-mark hold openhab-addons
 sudo systemctl start openhab.service
@@ -17,6 +17,6 @@ sudo systemctl enable openhab.service
 openhab-cli info
 sudo apt update                                        
 sudo apt upgrade                                       
-sudo apt-get install -y nodejs                            
-sudo apt-get install -y npm                               
-sudo npm i frontail -g -y
+sudo apt-get install --yes --force-yes nodejs                            
+sudo apt-get install --yes --force-yes npm                               
+sudo npm i frontail -g --yes --force-yes
