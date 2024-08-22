c190ac5b6371182d1fc10a44b22358626c0b276b
AndrejMeszarosDS <78271014+AndrejMeszarosDS@users.noreply.github.com>
2024-08-22 18:08:37 +0200

    make nice install script

--- openHab4131.sh
+++ openHab4131.sh
@@ -1,49 +1,46 @@
-# update & upgrade
+#--------------------------------------------------------------------------------------------------
+# update & upgrade                                                                                |
+#--------------------------------------------------------------------------------------------------
 #sudo apt-get update
 #sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 
-# install java 17
+#--------------------------------------------------------------------------------------------------
+# install java 17                                                                                 |
+#--------------------------------------------------------------------------------------------------
 sudo apt install --yes --force-yes openjdk-17-jre-headless
-# install openHAB 3.4.2.1
+
+#--------------------------------------------------------------------------------------------------
+# install openHAB 4.1.3.1                                                                         |
+#--------------------------------------------------------------------------------------------------
+opanhab_admin_user_name = admin
+opanhab_admin_user_password = openhab_password
 curl -fsSL "https://openhab.jfrog.io/artifactory/api/gpg/key/public" | gpg --dearmor > openhab.gpg
 sudo mkdir /usr/share/keyrings
 sudo mv openhab.gpg /usr/share/keyrings
 sudo chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg
 echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
-#sudo apt-get --yes --force-yes update
 sudo apt install --yes --force-yes openhab=4.1.3-1
 sudo apt-mark hold openhab
 sudo apt-mark hold openhab-addons
-# update addons.cfg ( mqtt-binding, persistence mapdb, influx )
-
-
-
-
-
 sudo systemctl start openhab.service
 sudo systemctl status openhab.service
 sudo systemctl daemon-reload
 sudo systemctl enable openhab.service
-openhab-cli info
+openhab-cli console -p habopen users add $opanhab_admin_user_name $opanhab_admin_user_password administrator
+# update addons.cfg ( mqtt-binding, persistence mapdb, influx )
 
-# add openhab admin user
-openhab-cli console -p habopen users add openhab openhab_password administrator
-
-
-
-
-
-
-
-# update & upgrade
+#--------------------------------------------------------------------------------------------------
+# update & upgrade                                                                                |
+#--------------------------------------------------------------------------------------------------
 #sudo apt-get update
 #sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 
-# install frontail and dependecies
+#--------------------------------------------------------------------------------------------------
+# install frontail and dependecies and make to work                                               |
+#--------------------------------------------------------------------------------------------------
 sudo apt-get install --yes --force-yes nodejs                            
 sudo apt-get install --yes --force-yes npm                               
 sudo npm i frontail -g --yes --force-yes
-# remake frontail to work
 cd ~/../../usr/local/lib/node_modules/frontail/web
 sudo rm index.html
 sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/index.html
@@ -64,35 +61,35 @@ sudo systemctl -q daemon-reload
 sudo systemctl enable --now frontail.service
 sudo systemctl restart frontail.service
 
-# update & upgrade
+#--------------------------------------------------------------------------------------------------
+# update & upgrade                                                                                |
+#--------------------------------------------------------------------------------------------------
 #sudo apt-get update
 #sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 
-# samba share
+#--------------------------------------------------------------------------------------------------
+# samba share                                                                                     |
+#--------------------------------------------------------------------------------------------------
+samba_share_password = secret
 sudo apt-get install --yes --force-yes samba samba-common-bin
-# samba load updated configuration
 cd ~/../../etc/samba/
 sudo rm smb.conf
 sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/samba/smb.conf
-# set user and password orangepi/secret
-pass="secret"
-(echo "$pass"; echo "$pass") | smbpasswd -s -a "$SUDO_USER"
-# solve permission problem
+(echo "$samba_share_password"; echo "$samba_share_password") | smbpasswd -s -a "$SUDO_USER"
 sudo usermod -a -G openhab orangepi
 sudo chmod -R g+w /etc/openhab
-# restart samba service
 sudo systemctl restart smbd.service
 
-#install Mosquitto Broker
+#--------------------------------------------------------------------------------------------------
+#install Mosquitto Broker                                                                         |
+#--------------------------------------------------------------------------------------------------
+mosquitto_password = mqttpass
 sudo apt-get install -y mosquitto mosquitto-clients
-# download new configuration
 cd ~/../../etc/mosquitto/
 sudo rm mosquitto.conf
 sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/mosquitto/mosquitto.conf
-# add user
-sudo mosquitto_passwd -b -c /etc/mosquitto/passwd orangepi mqttpass
+sudo mosquitto_passwd -b -c /etc/mosquitto/passwd orangepi $mosquitto_password
 sudo systemctl enable mosquitto.service
-# restart mosquitto
 sudo systemctl restart mosquitto
 
 
