42e2f759759bca2f4b3b926719b5e96522fa25b8
AndrejMeszarosDS <78271014+AndrejMeszarosDS@users.noreply.github.com>
2024-08-26 10:30:18 +0200

    adding persist files

--- add_test_data.sh
+++ add_test_data.sh
@@ -1,6 +1,10 @@
 cd ~/../../etc/openhab/items 
 sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/test_data/influx.items
 sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/test_data/mapdb.items
+
 cd ~/../../etc/openhab/things
 sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/test_data/influx.things
 
+cd ~/../../etc/openhab/persistence
+sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/openhab/influxdb.persist
+sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/openhab/mapdb.persist
