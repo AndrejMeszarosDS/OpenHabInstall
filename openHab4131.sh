fe4ed9574efb766bd0a35b1c2b35b891244a78d7
AndrejMeszarosDS <78271014+AndrejMeszarosDS@users.noreply.github.com>
2025-02-02 13:37:02 +0100

    test mosquitto install and setup script

--- openHab4131.sh
+++ openHab4131.sh
@@ -1,70 +1,70 @@
 #--------------------------------------------------------------------------------------------------
 # update & upgrade                                                                                |
 #--------------------------------------------------------------------------------------------------
-sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+# sudo apt-get update
+# sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 
 #--------------------------------------------------------------------------------------------------
 # install java 17                                                                                 |
 #--------------------------------------------------------------------------------------------------
-sudo apt install --yes --force-yes openjdk-17-jre-headless
+#sudo apt install --yes --force-yes openjdk-17-jre-headless
 
 #--------------------------------------------------------------------------------------------------
 # install openHAB 4.1.3.1                                                                         |
 #--------------------------------------------------------------------------------------------------
-curl -fsSL "https://openhab.jfrog.io/artifactory/api/gpg/key/public" | gpg --dearmor > openhab.gpg
-sudo mkdir /usr/share/keyrings
-sudo mv openhab.gpg /usr/share/keyrings
-sudo chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg
-echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
-sudo apt-get update
-sudo apt install --yes --force-yes openhab=4.1.3-1
-sudo apt-mark hold openhab
-sudo apt-mark hold openhab-addons
-sudo systemctl start openhab.service
-sudo systemctl status openhab.service
-sudo systemctl daemon-reload
-sudo systemctl enable openhab.service
+# curl -fsSL "https://openhab.jfrog.io/artifactory/api/gpg/key/public" | gpg --dearmor > openhab.gpg
+# sudo mkdir /usr/share/keyrings
+# sudo mv openhab.gpg /usr/share/keyrings
+# sudo chmod u=rw,g=r,o=r /usr/share/keyrings/openhab.gpg
+# echo 'deb [signed-by=/usr/share/keyrings/openhab.gpg] https://openhab.jfrog.io/artifactory/openhab-linuxpkg stable main' | sudo tee /etc/apt/sources.list.d/openhab.list
+# sudo apt-get update
+# sudo apt install --yes --force-yes openhab=4.1.3-1
+# sudo apt-mark hold openhab
+# sudo apt-mark hold openhab-addons
+# sudo systemctl start openhab.service
+# sudo systemctl status openhab.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable openhab.service
 
 #--------------------------------------------------------------------------------------------------
 # install frontail and dependecies and make to work                                               |
 #--------------------------------------------------------------------------------------------------
-sudo apt-get install --yes --force-yes nodejs                            
-sudo apt-get install --yes --force-yes npm                               
-sudo npm i frontail -g --yes --force-yes
-cd ~/../../usr/local/lib/node_modules/frontail/web
-sudo rm index.html
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/index.html
-cd ~/../../usr/local/lib/node_modules/frontail/web/assets
-sudo rm app.js
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/app.js
-cd ~/../../usr/local/lib/node_modules/frontail/web/assets/styles
-sudo rm bootstrap.min.css
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/bootstrap.min.css
-cd ~/../../usr/local/lib/node_modules/frontail/preset
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/openhab_AEM.json
-cd ~/../../usr/local/lib/node_modules/frontail/web/assets/styles
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/openhab_AEM.css
-cd ~/../../etc/systemd/system
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/frontail.service
-sudo chmod 644 /etc/systemd/system/frontail.service
-sudo systemctl -q daemon-reload
-sudo systemctl enable --now frontail.service
-sudo systemctl restart frontail.service
+# sudo apt-get install --yes --force-yes nodejs                            
+# sudo apt-get install --yes --force-yes npm                               
+# sudo npm i frontail -g --yes --force-yes
+# cd ~/../../usr/local/lib/node_modules/frontail/web
+# sudo rm index.html
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/index.html
+# cd ~/../../usr/local/lib/node_modules/frontail/web/assets
+# sudo rm app.js
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/app.js
+# cd ~/../../usr/local/lib/node_modules/frontail/web/assets/styles
+# sudo rm bootstrap.min.css
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/bootstrap.min.css
+# cd ~/../../usr/local/lib/node_modules/frontail/preset
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/openhab_AEM.json
+# cd ~/../../usr/local/lib/node_modules/frontail/web/assets/styles
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/openhab_AEM.css
+# cd ~/../../etc/systemd/system
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/frontail/frontail.service
+# sudo chmod 644 /etc/systemd/system/frontail.service
+# sudo systemctl -q daemon-reload
+# sudo systemctl enable --now frontail.service
+# sudo systemctl restart frontail.service
 
 #--------------------------------------------------------------------------------------------------
 # samba share                                                                                     |
 #--------------------------------------------------------------------------------------------------
-samba_share_password=secret
-sudo apt-get install --yes --force-yes samba samba-common-bin
-cd ~/../../etc/samba/
-sudo rm smb.conf
-sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/samba/smb.conf
-(echo "$samba_share_password"; echo "$samba_share_password") | smbpasswd -s -a "$SUDO_USER"
-sudo usermod -a -G openhab orangepi
-sudo chmod -R g+w /etc/openhab
-sudo chmod -R g+w /var/lib/openhab
-sudo systemctl restart smbd.service
+# samba_share_password=secret
+# sudo apt-get install --yes --force-yes samba samba-common-bin
+# cd ~/../../etc/samba/
+# sudo rm smb.conf
+# sudo wget https://raw.githubusercontent.com/AndrejMeszarosDS/OpenHabInstall/main/samba/smb.conf
+# (echo "$samba_share_password"; echo "$samba_share_password") | smbpasswd -s -a "$SUDO_USER"
+# sudo usermod -a -G openhab orangepi
+# sudo chmod -R g+w /etc/openhab
+# sudo chmod -R g+w /var/lib/openhab
+# sudo systemctl restart smbd.service
 
 #--------------------------------------------------------------------------------------------------
 # install Mosquitto Broker                                                                        |
@@ -81,33 +81,33 @@ sudo systemctl restart mosquitto
 #--------------------------------------------------------------------------------------------------
 # create openhab admin user                                                                       |
 #--------------------------------------------------------------------------------------------------
-opanhab_admin_user_name="admin"
-opanhab_admin_user_password="openhab_password"
-openhab-cli console -p habopen users add $opanhab_admin_user_name $opanhab_admin_user_password administrator
+# opanhab_admin_user_name="admin"
+# opanhab_admin_user_password="openhab_password"
+# openhab-cli console -p habopen users add $opanhab_admin_user_name $opanhab_admin_user_password administrator
 
 #--------------------------------------------------------------------------------------------------
 # install influx                                                                                  |
 #--------------------------------------------------------------------------------------------------
-curl -LO https://download.influxdata.com/influxdb/releases/influxdb2_2.7.7-1_arm64.deb
-sudo dpkg -i influxdb2_2.7.7-1_arm64.deb
-sudo service influxdb start
+# curl -LO https://download.influxdata.com/influxdb/releases/influxdb2_2.7.7-1_arm64.deb
+# sudo dpkg -i influxdb2_2.7.7-1_arm64.deb
+# sudo service influxdb start
 #sudo service influxdb status ???
 
 #--------------------------------------------------------------------------------------------------
 # install influx CLI                                                                              |
 #--------------------------------------------------------------------------------------------------
-wget https://download.influxdata.com/influxdb/releases/influxdb2-client-2.7.5-linux-arm64.tar.gz
-tar xvzf ./influxdb2-client-2.7.5-linux-arm64.tar.gz
+# wget https://download.influxdata.com/influxdb/releases/influxdb2-client-2.7.5-linux-arm64.tar.gz
+# tar xvzf ./influxdb2-client-2.7.5-linux-arm64.tar.gz
 
 #--------------------------------------------------------------------------------------------------
 # create influx admin user and database                                                           |
 #--------------------------------------------------------------------------------------------------
-./influx setup \
-  --username influx_admin \
-  --password influx_admin_password \
-  --org openhab \
-  --bucket openhab_db \
-  --force
+# ./influx setup \
+#   --username influx_admin \
+#   --password influx_admin_password \
+#   --org openhab \
+#   --bucket openhab_db \
+#   --force
 
   
 
