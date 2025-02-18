5cfb02d283554415fe05dc350fe6b2220ef35592
AndrejMeszarosDS <78271014+AndrejMeszarosDS@users.noreply.github.com>
2025-02-18 10:58:48 +0100

    added cli

--- install_influx.sh
+++ install_influx.sh
@@ -10,22 +10,25 @@ INFLUXDB_RETENTION="0" # Infinite retention
 #--------------------------------------------------------------------------------------------------
 # update & upgrade                                                                                |
 #--------------------------------------------------------------------------------------------------
-sudo apt-get update
-sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
+#sudo apt-get update
+#sudo apt-get -o Dpkg::Options::="--force-confnew" --force-yes -fuy dist-upgrade
 
 
 #--------------------------------------------------------------------------------------------------
 # install influx                                                                                  |
 #--------------------------------------------------------------------------------------------------
-cd ~
 curl -LO https://download.influxdata.com/influxdb/releases/influxdb2_2.7.7-1_arm64.deb
 sudo dpkg -i influxdb2_2.7.7-1_arm64.deb
 sudo service influxdb start
+
+wget https://download.influxdata.com/influxdb/releases/influxdb2-client-2.7.5-linux-arm64.tar.gz
+tar xvzf ./influxdb2-client-2.7.5-linux-arm64.tar.gz
+
 echo "Waiting for InfluxDB to start..."
-sleep 10
+sleep 5
 
 echo "Setting up InfluxDB admin user..."
-influx setup --username "$INFLUXDB_USER" \
+./influx setup --username "$INFLUXDB_USER" \
              --password "$INFLUXDB_PASSWORD" \
              --org "$INFLUXDB_ORG" \
              --bucket "$INFLUXDB_BUCKET" \
@@ -33,10 +36,10 @@ influx setup --username "$INFLUXDB_USER" \
              --force
 
 echo "Creating OpenHAB user..."
-influx user create --name "$OPENHAB_USER" --password "$OPENHAB_PASSWORD"
+./influx user create --name "$OPENHAB_USER" --password "$OPENHAB_PASSWORD"
 
 echo "Granting OpenHAB user read/write permissions on the bucket..."
-influx auth create --user "$OPENHAB_USER" --write-buckets --read-buckets
+./influx auth create --user "$OPENHAB_USER" --write-buckets --read-buckets
 
 echo "Allowing password authentication..."
 sudo tee /etc/influxdb/config.toml <<EOF >/dev/null
